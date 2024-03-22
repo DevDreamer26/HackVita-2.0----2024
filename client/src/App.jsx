08140feeebe9c484a2e73da355dd5a2c0007cc15
Bidyasagar <bidyasagarhazarika@gmail.com>
2024-03-22 20:31:24 +0530

    Added Gemini powered chat bot

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import {BrowserRouter,Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import Home from './components/Home/Home'
 import About from "./components/About/About"
 import Navbar from './components/Navbar/Navbar'
@@ -7,22 +7,28 @@ import Contact from './components/Contact/Contact'
 import './App.css'
 import Footer from './components/Footer/Footer'
 import Hero from './components/Hero/Hero'
+import ChatWindow from './components/ChatWindow'
 
 function App() {
-  
 
   return (
     <>
-    <BrowserRouter>
-    <Navbar/>
-    <Routes>
-    <Route path="/" element={<Hero />} />
-    <Route path="/about" element={<About />} />
-    <Route path="/contact" element={<Contact />} />
-    </Routes>
-    <Home />  
-   <Footer/>
-   </BrowserRouter>
+      <BrowserRouter>
+        <Navbar />
+        <div className="content-wrapper">
+          <div className="sticky-button">
+            <Link to="/chatbot"><button>Chat with Us</button></Link>
+          </div>
+          <Routes>
+            <Route path="/" element={<Hero />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/chatbot" element={<ChatWindow />} />
+          </Routes>
+          <Home />
+          <Footer />
+        </div>
+      </BrowserRouter>
     </>
   )
 }
