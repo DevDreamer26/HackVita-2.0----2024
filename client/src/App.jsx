f9c145aa050ff38dee950068a4dc26b2bc0d1714
Bidyasagar <bidyasagarhazarika@gmail.com>
2024-03-22 18:45:29 +0530

    Navbar lol lmao xddd

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,12 @@
 import { useState } from 'react'
-import {BrowserRouter} from 'react-router-dom'
+import {BrowserRouter,Routes, Route} from 'react-router-dom'
 import Home from './components/Home/Home'
+import About from "./components/About/About"
 import Navbar from './components/Navbar/Navbar'
+import Contact from './components/Contact/Contact'
 import './App.css'
 import Footer from './components/Footer/Footer'
+import Hero from './components/Hero/Hero'
 
 function App() {
   
@@ -12,6 +15,11 @@ function App() {
     <>
     <BrowserRouter>
     <Navbar/>
+    <Routes>
+    <Route path="/" element={<Hero />} />
+    <Route path="/about" element={<About />} />
+    <Route path="/contact" element={<Contact />} />
+    </Routes>
     <Home />  
    <Footer/>
    </BrowserRouter>
