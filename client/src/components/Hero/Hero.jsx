d07b77714f7a5621d9d3b4d36b0e5752e8c75b7f
Chinmoy Koch <chinmoykoch12@gmail.com>
2024-03-22 18:47:53 +0530

    ee

--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import "./Hero.scss"
 
 const Hero = () => {
   return (
-    <>Hero</>
+    <div class="content">
+    <div class="container">
+      <div class="info">
+        <h1>Looking For Inspiration</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus odit nihil ullam nesciunt quidem iste, Repellendus odit nihil</p>
+        <button>Button name</button>
+      </div>
+      <div class="image">
+        <img src="https://i.postimg.cc/65QxYYzh/001234.png"/>
+      </div>
+    </div>
+  </div>
   )
 }
 
