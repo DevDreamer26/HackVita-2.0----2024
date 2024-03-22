855f3532cb0a250288a7a52feaf640abac2c5f5f
Chinmoy Koch <chinmoykoch12@gmail.com>
2024-03-22 18:55:35 +0530

    hero added

--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -4,17 +4,17 @@ import "./Hero.scss"
 const Hero = () => {
   return (
     <div class="content">
-    <div class="container">
-      <div class="info">
-        <h1>Looking For Inspiration</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus odit nihil ullam nesciunt quidem iste, Repellendus odit nihil</p>
-        <button>Button name</button>
-      </div>
-      <div class="image">
-        <img src="https://i.postimg.cc/65QxYYzh/001234.png"/>
+      <div class="container">
+        <div class="info">
+          <h1>Looking For Inspiration</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus odit nihil ullam nesciunt quidem iste, Repellendus odit nihil</p>
+          <button>Button name</button>
+        </div>
+        <div class="image">
+          <img src="images/hero-img.png"/>
+        </div>
       </div>
     </div>
-  </div>
   )
 }
 
